677bfdbc1c77a1d2d3b18e5ed31d8afbcd7ab6b7
Arivas29 <angelrivas2903@gmail.com>
2026-05-07 17:30:16 -0400

    creacion de la plantilla del proyecto

---
+++ config.py
@@ -0,0 +1,13 @@
+# CONSTANTES
+
+ANCHO, ALTO = 640, 480
+FPS = 60
+TITULO = 'Project Arcade'
+BACK_COLOR = (18, 180, 95)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PLAYER_IMG = 'src/manzanadorada.png' 
+ENEMY_IMG = 'src/'
+WIN_IMG = 'src/'
+BG_IMG = 'src/manzanavenenosa.png'
+TEXT_FONT = 'src/'
