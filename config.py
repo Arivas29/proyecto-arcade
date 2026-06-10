878ab30cf6a029f94f6c34e592fa17f48f198a43
Arivas29 <angelrivas2903@gmail.com>
2026-06-10 14:59:21 -0400

    Update screen dimensions and player image

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # CONSTANTES
 
-ANCHO, ALTO = 640, 480
+ANCHO, ALTO = 1150, 750
 FPS = 60
 TITULO = 'Project Arcade'
 BACK_COLOR = (18, 180, 95)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 PLAYER_IMG = 'src/maduro.png' 
-PLAYER_IMG2 = 'src/elonmusk.png'
+PLAYER_IMG2 = 'src/donaltrump.png'
 ENEMY_IMG = 'src/'
 WIN_IMG = 'src/'
 BG_IMG = 'src/espaciofondo.png'
